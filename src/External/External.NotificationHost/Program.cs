e467547caf89d00fd534bcae9f4ab3e1f642981e
Abdullah Tariq <abdullah.tariq@shohoz.com>
2021-09-22 06:24:49 +0600

    Project Initiation

---
+++ src/External/External.NotificationHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MontyHallProblemSimulation.External.NotificationHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/QueryWebHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MontyHallProblemSimulation.ReadSide.QueryWebHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
